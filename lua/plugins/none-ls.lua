c58afd1d9e0fb96b1fb2ca352252cac7b668816d
genedx-stanislavpinchuk <spinchuk-cw@genedx.com>
2024-05-13 21:46:48 +0300

    update none-ls and themes

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.completion.spell,
+                -- null_ls.builtins.completion.spell,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.diagnostics.eslint_d,
                 -- require("none-ls.diagnostics.eslint_d"),
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("catppuccin-latte")
+		-- end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		name = "gruvbox",
+		priority = 1000,
+		-- config = function()
+		-- 	vim.o.background = "light"
+		-- 	vim.cmd.colorscheme("gruvbox")
+		-- end,
+	},
+	{
+		"maxmx03/solarized.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.o.background = "light"
+			vim.cmd.colorscheme("solarized")
+		end,
+	},
+}
